5654b9b2690e7f6ceff18a0097680ba383510649
Juan Rodriguez <phjuanrodriguez@gmail.com>
2023-11-18 17:38:33 -0300

    changes in enviroments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'digi-api.com/api/v1/digimon',
     localApiUrl: 'http://localhost:3000/',
     apiAgify: 'https://api.agify.io/'
   };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://www.digi-api.com/api/v1/digimon/',
     localApiUrl: 'http://localhost:3000/',
     apiAgify: 'https://api.agify.io/'
   };
